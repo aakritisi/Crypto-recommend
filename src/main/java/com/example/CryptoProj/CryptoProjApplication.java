de1a7d90aa1051574edc139b83e8eba1e8f00e1a
aakritisingh <aakritisingh@10.230.44.13>
2021-10-16 02:30:38 -0600

    Init commit

---
+++ src/main/java/com/example/CryptoProj/CryptoProjApplication.java
@@ -0,0 +1,13 @@
+package com.example.CryptoProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CryptoProj/CryptoProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CryptoProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
